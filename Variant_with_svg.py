3b8b5acb133824eb55cf44e7b875fe18896af31e
d.mastitskiy <dimulgator@outlook.com>
2022-01-24 12:21:38 +0300

    It's okey now

--- Variant_with_svg.py
+++ Variant_with_svg.py
@@ -23,42 +23,42 @@ def qr_img():
     img = qrcode.make(data, image_factory=factory)
 
     img.save("qrcode.svg")
-    saved_qr = "qrcode.svg"
+    saved_qr = "qrcode.svg" #saved qr-code
     return saved_qr
 
 
 #convertation
 
 def conv_pdf():
-    pppdddfff = convert("Test1.docx")
+    pppdddfff = convert("Cover Letter.docx") #main docx file
     return pppdddfff
 
+#insert svg to pdf
 
 def insert_to_pdf():
     image_path = "qrcode.svg" #
     filename = "file_for_svg.pdf" # input file
     my_pdf = canvas.Canvas(filename)
     drawing = svg2rlg(image_path)
-    renderPDF.draw(drawing, my_pdf, 240, 30)
-    my_pdf.save()
+    renderPDF.draw(drawing, my_pdf, 265, 30) #coordinates where the image will be
+    my_pdf.save() #save the new file with image (.pdf)
 
+#merge 2 pdf
 
-
-    with open("Test1.pdf",  "rb") as inFile, open("file_for_svg.pdf", "rb") as overlay:
+    with open("Cover Letter.pdf",  "rb") as inFile, open("file_for_svg.pdf", "rb") as overlay: #open 2 files
         original = pypdf.PdfFileReader(inFile)
 
         writer = pypdf.PdfFileWriter()
 
 
-        for i in range(original.getNumPages()):
-            background = original.getPage(i)
-            foreground = pypdf.PdfFileReader(overlay).getPage(0)
+        for current_page in range(original.getNumPages()): #cicle for taking the pages
+            background = original.getPage(current_page) #taking the pages from the main document
+            foreground = pypdf.PdfFileReader(overlay).getPage(0) #read the overlay
             background.mergePage(foreground)
-            page = original.getPage(i)
+            page = original.getPage(current_page)
             writer.addPage(page)
-
-    with open("test_svg_file.pdf", "wb") as outFile:
-        writer.write(outFile)
+            with open("test_svg_file.pdf", "wb") as outFile:
+                writer.write(outFile) #output file
 
 qr_img()
 conv_pdf()
